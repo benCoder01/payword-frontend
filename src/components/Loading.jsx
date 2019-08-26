a6a7501956553e10f5b4c28a8d199ea9b7984b65
Benedikt Ricken <benedikt-ricken@gmx.de>
2019-08-26 15:07:04 +0200

    add fade component to display later

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Fade from "@material-ui/core/Fade";
 
 const styles = {
   root: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 };
 
 function Loading(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-        {props.loading &&
+      <Fade in={props.loading}>
         <LinearProgress color="secondary" variant="query" />
-        }
+      </Fade>
     </div>
   );
 }
 
 Loading.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Loading);
